a2ebe7840cc2a13cf775d1e18225424927ca1117
vanpe1t <makarov.maks.v@yandex.ru>
2023-03-13 08:56:55 +0300

    Initial commit

---
+++ src/main/java/ru/makarov/recipesapp/controllers/MainController.java
@@ -0,0 +1,2 @@
+package ru.makarov.recipesapp.controllers;public class MainController {
+}
---
+++ src/test/java/ru/makarov/recipesapp/RecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.makarov.recipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
