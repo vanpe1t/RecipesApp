567519c32c75c0fdc39ccb667ab9f0b2f61b8a66
vanpe1t <makarov.maks.v@yandex.ru>
2023-03-13 09:18:59 +0300

    home_task

--- src/main/java/ru/makarov/recipesapp/controllers/MainController.java
+++ src/main/java/ru/makarov/recipesapp/controllers/MainController.java
@@ -1,2 +1,20 @@
-package ru.makarov.recipesapp.controllers;public class MainController {
+package ru.makarov.recipesapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String GetString() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String GetInfo() {
+        return "Makarov Maxim; " + "Project name: Recipes application; " + "Creation date: 01-03-2023; " +
+                "Description: приложение по работе с рецептами."   ;
+    }
+
 }
